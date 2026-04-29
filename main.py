9a39f54e8aadfc0512bf86796266f7011c4ff9a9
Durga0592 <durgarao@fliptrx.com>
2026-04-30 00:49:04 +0530

    app name updated..

--- main.py
+++ main.py
@@ -8,11 +8,6 @@ client = bigquery.Client()
 
 @app.route("/")
 def main(big_query_client=client):
-    """
-    Docstring for main
-    
-    :param big_query_client: Description
-    """
     table_id = "mlops-1-494812.test_schema.us_states"
     job_config = bigquery.LoadJobConfig(
         write_disposition=bigquery.WriteDisposition.WRITE_TRUNCATE,
